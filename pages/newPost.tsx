50ae54c11043c9ee96d601122974fdbb9f9a2627
Aleksandr <a13x.student@yandex.ru>
2021-01-22 11:40:26 +0300

    new post page styles added

--- pages/newPost.tsx
+++ pages/newPost.tsx
@@ -2,26 +2,49 @@ import { FormEvent, useState } from 'react'
 import { CustomHead } from '../components/CustomHead'
 
 const NewPost = () => {
-    const [postBody, setPostBody] = useState('');
-    const [postTitle, setPostTitle] = useState('');
-    const submitForm = (e: FormEvent<HTMLFormElement>) => {
+    const [postTitle, setPostTitle] = useState('')
+    const [postBody, setPostBody] = useState('')
+    const submitForm = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const post = {
             title: postTitle,
             body: postBody
         }
-        fetch(`http://localhost:3000/api/newPost`, {
-            method: 'POST',
-            body: JSON.stringify(post)
-        })
+        try {
+            await fetch(`http://localhost:3000/api/newPost`, {
+                method: 'POST',
+                body: JSON.stringify(post)
+            })
+            setPostTitle('')
+            setPostBody('')
+        } catch(e) {
+            alert(e)
+        }
     }
     return <>
-        <CustomHead title='Create New Post'/>
+        <CustomHead title='Create New Post' />
         <main className='main'>
-            <form onSubmit={submitForm}>
-                <input type='text' name='postTitle' onChange={(e) => setPostTitle(e.target.value)} value={postTitle}/>
-                <textarea value={postBody} onChange = {(e) => setPostBody(e.target.value)} name='postBody'></textarea>
-                <button>Send form</button>
+            <form className='main__form newPostForm' onSubmit={submitForm}>
+                <label>
+                    Post title:
+                    <input type='text'
+                        id='postTitle'
+                        name='postTitle'
+                        className='newPostForm__inputTitle'
+                        required={true}
+                        autoComplete='off'
+                        onChange={(e) => setPostTitle(e.target.value)} value={postTitle} />
+                </label>
+                <label className='main__bodyLabel'>
+                    Post body:
+                    <textarea value={postBody}
+                        id='postBody'
+                        name='postBody'
+                        className='newPostForm__inputBody'
+                        required={true}
+                        onChange={(e) => setPostBody(e.target.value)}></textarea>
+                </label>
+                <button className='newPostForm__button'>Send form</button>
             </form>
         </main>
     </>
