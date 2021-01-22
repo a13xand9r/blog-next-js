1f409dbac43c13be68bf820b361b16e8ba85522a
Aleksandr <a13x.student@yandex.ru>
2021-01-22 14:14:32 +0300

    added auto generated id

--- pages/api/newPost.ts
+++ pages/api/newPost.ts
@@ -4,7 +4,7 @@ import { addPost, PostType } from '../../mockDataBase'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const post: PostType = {...JSON.parse(req.body), date: new Date().toLocaleString(), id: Math.floor(Math.random() * 10000).toString()}
+    const post: PostType = {...JSON.parse(req.body), date: new Date().toLocaleString(), id: v4()}
     post.body = ['<p>', ...post.body.split('').map(item => {
         if (item === '\n') return '</p> <p>'
         else return item
